7d292b494a65babc49e5a7a115f3008f8df012e1
kanan132 <63307356+kanan132@users.noreply.github.com>
2020-05-20 21:15:30 +0400

    Add files via upload

--- weather oop/js/json.js
+++ weather oop/js/json.js
@@ -79,14 +79,48 @@ class CurrentWeather {
 
     getting_features(keyword){
         
-        let last_searched_city=JSON.parse(localStorage.getItem(keyword));
-        for(let p in last_searched_city ){
-            document.querySelector("#last").innerHTML+=`${last_searched_city[p].name}`;
-        }
-        /*let last_searched_city=localStorage.getItem(keyword).split(",");
-        for(let i=0;i<last_searched_city.length;i++){
-            document.querySelector(".table").innerHTML+="<tr><td>"+ last_searched_city[i].keyword+ "</td><td>" + last_searched_city[i].get_temp+ "</td><td>" + last_searched_city[i].get_weather+"</td></tr>";
+        let searched_city=JSON.parse(localStorage.getItem("lastCity"));
+        //document.querySelector("#last").innerHTML=searched_city.name;
+        document.querySelector("#last").innerHTML=`<div class="col-md-6">
+             
+            <ul>   
+                <h4>Name:</h4><li class="city forward"  >
+                    ${searched_city.name}
+                </li>
+            </ul> 
+            <ul>
+                <h4>Weather:</h4><li class="weather forward">
+                    ${searched_city.weather[0].description}
+                </li>
+            </ul>
+            <ul>
+                <h4>Main:</h4><li class="main forward">
+                   temp: ${searched_city.main.temp} K
+                </li>
+            </ul>`
+        
+        /*let searched_city=localStorage.getItem(keyword).split(",");
+        for(let i=0;i<searched_city.length;i++){
+            document.querySelector(".table").innerHTML+="<tr><td>"+ searched_city[i].keyword+ "</td><td>" + searched_city[i].get_temp+ "</td><td>" + searched_city[i].get_weather+"</td></tr>";
         }*/
+
+    }
+
+    filling_array(keyword){
+        let last_searches = localStorage.getItem(keyword);
+        if (last_searches == undefined) {
+        last_searches = [];
+        } else {
+        last_searches = last_searches.split(",");
+        }
+    }
+
+    changing_city(){
+        if (last_searches.length !== 0){
+            let last_value = last_searches[last_searches.length-1];
+            localStorage.setItem("lastCity",last_value);
+            
+        }   
     }
     
 }
