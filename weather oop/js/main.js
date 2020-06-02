458d06eb5ac3f8dd538074652bd3301f415e2ffd
kanan132 <63307356+kanan132@users.noreply.github.com>
2020-06-02 20:05:48 +0400

    Add files via upload

--- weather oop/js/main.js
+++ weather oop/js/main.js
@@ -7,7 +7,6 @@ function timestamp_to_date (seconds){
     return new Date(seconds * 1000).toISOString().slice(0, 19).replace('T', ' ');
 }
 
-
 function get_records() {
     let records = [];
     for(let i = 0; i < window.localStorage.length; i++) {
@@ -27,15 +26,11 @@ function get_records() {
     return records;
 }
 
-
-
-
 document.querySelector("#submit-button").addEventListener("click", () => {
     let  keyword = document.querySelector("#keyword").value;
     weather.search(keyword);
     
 });
-
 
 function load_data() {
 
@@ -45,18 +40,17 @@ function load_data() {
 	let table_data = records.map(record => {
 		return `
         <tr class="table-city">
-            <td>
+           
+            <td class="reuse">
                ${record.city.split("_")[1]} 
+            </td>
+            <td>
+                ${Math.round(record.data.main.temp - 273.15)}*C
             </td>
             <td>
                 ${record.data.weather[0].description}
             </td>
-            <td>
-                Other thing
-            </td>
-            <td>
-                Yet other thing;
-            </td>
+        
         </tr>`;
 	}).join("");
 	document.getElementById("cities").innerHTML = table_data;
@@ -64,3 +58,10 @@ function load_data() {
 
 window.load = load_data();
 
+function touching_city(){ 
+    document.querySelector(".reuse").addEventListener('click',(e) => {
+        let  value=e.target.value;
+        let result=weather.search(value);
+        return weather.render_data_to_dom(result.data);
+    })
+}
